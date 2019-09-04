d0a30e203a72551200a7a98ac6581a9e92de8994
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2019-09-04 15:52:47 -0500

    set up project, added user POST route

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API RUNNING"));
+
+// Define Routes
+app.use("/api/users", require("./routes/api/users"));
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
