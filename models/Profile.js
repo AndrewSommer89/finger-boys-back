33439ea86efa953c9af9ffe0739b231933643527
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2019-09-04 17:07:52 -0500

    added profile model

---
+++ models/Profile.js
@@ -0,0 +1,91 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  leagueMember: {
+    type: Boolean
+  },
+  phone: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  stats: [
+    {
+      averageScore: {
+        type: Number
+      },
+      highScore: {
+        type: Number
+      },
+      whiteRussiansBought: {
+        type: Number
+      },
+      gamesBowled: {
+        type: Number
+      },
+      framesBowled: {
+        type: Number
+      },
+      strikes: {
+        type: Number
+      },
+      strikePercentage: {
+        type: Number
+      },
+      spares: {
+        type: Number
+      },
+      sparePercentage: {
+        type: Number
+      },
+      firstBallAverage: {
+        type: Number
+      },
+      openFramePercentage: {
+        type: Number
+      },
+      singlePinPickupPercentage: {
+        type: Number
+      },
+      totalSplits: {
+        type: Number
+      },
+      totalSplitsConverted: {
+        type: Number
+      },
+      splitConversionRate: {
+        type: Number
+      },
+      gutterballs: {
+        type: Number
+      },
+      gutterballPercentage: {
+        type: Number
+      },
+      under100: {
+        type: Number
+      },
+      above175: {
+        type: Number
+      },
+      above200: {
+        type: Number
+      },
+      above225: {
+        type: Number
+      },
+      above275: {
+        type: Number
+      },
+      legendsClub: {
+        type: Boolean,
+        type: Number
+      }
+    }
+  ]
+});
