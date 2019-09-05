0dfb54d504299ae0ac06744f04e4062f678123c6
Andrew Sommere <andrew.conrad.sommer@gmail.com>
2019-09-05 01:15:42 -0500

    added all routes for posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -226,4 +226,27 @@ router.put(
     }
   }
 );
+
+// @route   DELETE api/profile/score/:score_id
+// @desc    Delete score from profile
+// @access  Private
+router.delete("/score/:score_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.scores
+      .map(item => item.id)
+      .indexOf(req.params.score_id);
+
+    profile.scores.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 module.exports = router;
